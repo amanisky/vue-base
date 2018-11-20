d594959ac73cf7099adc9145c7f4b964343a07d9
amanisky <zhangfand@isoftstone.com>
2018-11-20 17:04:51 +0800

    新增api

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_BASE_URL: '"/api"'
+  API_BASE_URL: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
 })
